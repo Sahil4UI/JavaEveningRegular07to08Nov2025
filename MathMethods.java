18487f1c027139cb5352005515f8f892dc8500b8
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-11-28 19:28:27 +0530

    Create MathMethods.java

---
+++ MathMethods.java
@@ -0,0 +1,25 @@
+
+public class MathMethods{
+	public static void main(String[] args) {
+		
+//		System.out.println("Hello\nWorld!!!");
+//		System.out.println("Hello\tWorld!!!");
+////		\"  \" - string
+//		System.out.println("Hello \'World\'!!!");
+//		System.out.println("C:\\Users\\Vanshika");
+		int x = 4,y = 2;
+//		int res = Math.abs(Math.pow(x, 2)+x*y);
+		
+//		System.out.println(Math.pow(2, 3));
+//		System.out.println(Math.floor(2.435));
+//		System.out.println(Math.ceil(2.435));
+//		System.out.println(Math.abs(-324));
+//		System.out.println(Math.min(99,81));
+//		System.out.println(Math.max(99,81));
+//		System.out.println(Math.sqrt(2));
+//		System.out.println(Math.cbrt(27));
+//		System.out.println(Math.round(2.234));
+//		System.out.println((int)(Math.random()*10));
+	
+	}
+}
